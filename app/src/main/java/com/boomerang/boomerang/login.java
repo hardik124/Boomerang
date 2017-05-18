ef0336aa38570b368fc17dc2b75523d2114b9d88
Hardik <hardik12498@gmail.com>
2017-05-18 23:21:44 +0530

    Initial commit

---
+++ app/src/main/java/com/boomerang/boomerang/login.java
@@ -0,0 +1,15 @@
+package com.boomerang.boomerang;
+
+import android.os.Bundle;
+
+import com.boomerang.boomerang.base.BaseActivity;
+
+
+public class login extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/boomerang/boomerang/view/BaseActivityCallback.java
@@ -0,0 +1,12 @@
+package com.boomerang.boomerang.view;
+
+import android.content.Context;
+
+public interface BaseActivityCallback {
+
+    void showProgressDialog(String message, Context ctx);
+
+    void hideProgressDialog();
+
+    void setToolbarTitle(String title);
+}
